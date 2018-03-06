a6170c233488cdc51e06e83d4f59dc5daf7a4fe6
Sebastien Besson <seb.besson@gmail.com>
2018-03-06 13:39:59 +0000

    Comments

--- make_patterns.py
+++ make_patterns.py
@@ -6,20 +6,26 @@ from os.path import basename, join
 from pyidr.file_pattern import FilePattern
 
 TIMEPOINTS = 40
+BASE_DIRECTORY = "/uod/idr/filesets/idr0041-cai-mitoticatlas/"
 
-base = "/uod/idr/filesets/idr0041-cai-mitoticatlas/"
+# Determine base location for pattern files
 patterns_base = join(os.path.dirname(os.path.realpath(__file__)), "patterns")
 print "Creating patterns from %s, saving under %s" % (base, patterns_base)
 
-assays = [join(base, x) for x in os.listdir(base)]
+# List all assay folders under base directory
+assays = [join(BASE_DIRECTORY, x) for x in os.listdir(base)]
 assays = filter(os.path.isdir, assays)
 for assay in assays:
+    # Retrieve individual cell per assays excluding calibration folders
     cells = [x for x in glob(assay + "/*") if not x.endswith("Calibration")]
     for cell in cells:
+        # List raw and concatenated TIFF files
         rawtifs = sorted([x for x in glob(cell + "/rawtif/*")
                           if not x.endswith("Thumbs.db")])
         conctifs = sorted([x for x in glob(cell + "/conctif/*")
                            if not x.endswith("Thumbs.db")])
+
+        # Construct and validate pattern strings
         raw_pattern = join(cell, "rawtif",
             basename(rawtifs[0])[:-6] + "<01-%g>" % TIMEPOINTS + ".tif")
         assert list(FilePattern(raw_pattern).filenames()) == rawtifs
@@ -27,11 +33,12 @@ for assay in assays:
             basename(rawtifs[0])[:-6] + "<01-%g>" % TIMEPOINTS + ".tif")
         assert list(FilePattern(conc_pattern).filenames()) == conctifs
 
+        # Create pattern files on disk
         pattern_dir = join(patterns_base, basename(assay), basename(cell))
         if not os.path.exists(pattern_dir):
             os.makedirs(pattern_dir)
-        raw_pattern_file =  join(pattern_dir, "rawtif.pattern")
-        conc_pattern_file =  join(pattern_dir, "conctif.pattern")
+        raw_pattern_file = join(pattern_dir, "rawtif.pattern")
+        conc_pattern_file = join(pattern_dir, "conctif.pattern")
         with open(raw_pattern_file, 'w') as f:
             print "Writing %s" % raw_pattern_file
             f.write(raw_pattern)
