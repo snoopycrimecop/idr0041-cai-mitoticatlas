f0e587e2a2f504afd9eb58626e664ae0e32f598d
Sebastien Besson <seb.besson@gmail.com>
2018-03-06 12:33:09 +0000

    Add first version of script to generate pattern files

---
+++ make_patterns.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from glob import glob
+import os
+from os.path import basename, join
+from pyidr.file_pattern import FilePattern
+
+TIMEPOINTS = 40
+
+base = "/uod/idr/filesets/idr0041-cai-mitoticatlas/"
+plates = "idr-metadata/idr0041-cai-mitoticatlas/patterns/"
+
+assays = [join(base, x) for x in os.listdir(base)]
+assays = filter(os.path.isdir, assays)
+for assay in assays:
+    cells = [x for x in glob(assay + "/*") if not x.endswith("Calibration")]
+    if not cells:
+        print "Empty directory: %s" % assay
+        continue
+    for cell in cells:
+        rawtifs = sorted(glob(cell + "/rawtif/*"))
+        assert len(rawtifs) == TIMEPOINTS, (
+            "Incorrect number of files on %s" % cell)
+        pattern = join(cell, "rawtif",
+            basename(rawtifs[0])[:-6] + "<01-%g>" % TIMEPOINTS + ".tif")
+        
+        assert list(FilePattern(pattern).filenames()) == rawtifs
