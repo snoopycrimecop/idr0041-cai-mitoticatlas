71da070cb73eff18a6f35eeaeca6a6c5aef9e79f
Sebastien Besson <seb.besson@gmail.com>
2018-03-20 10:39:04 +0000

    Add logging

--- scripts/make_filePaths_from_original_data.py
+++ scripts/make_filePaths_from_original_data.py
@@ -2,6 +2,7 @@
 # Generate filePaths tsv file with raw files
 
 from glob import glob
+import logging
 import os
 from os.path import basename, join
 import sys
@@ -15,6 +16,7 @@ IMAGE_TYPES = {
     'conc': 'conctif',
 }
 
+logging.basicConfig(level=logging.INFO)
 if not os.path.exists(BASE_DIRECTORY):
     print "Cannot find the raw data directory. Exiting."
     sys.exit(0)
@@ -26,18 +28,22 @@ filepaths_file = join(metadata_dir, "..", "experimentA",
 
 if os.path.exists(filepaths_file):
     os.remove(filepaths_file)
-print "Creating %s" % filepaths_file
+logging.info("Deleting %s" % filepaths_file)
 
 # List all assay folders under base directory
 assays = [join(BASE_DIRECTORY, x) for x in os.listdir(BASE_DIRECTORY)]
 assays = sorted(filter(os.path.isdir, assays))
 for assay in assays[:LIMIT]:
+    logging.info("Finding cells under %s" % assay)
     # Retrieve individual cell per assays excluding calibration folders
     cells = [x for x in glob(assay + "/*") if not x.endswith("Calibration")]
     for cell in cells:
         for t in IMAGE_TYPES:
-            tifs = sorted([x for x in glob(cell + "/%s/*" % IMAGE_TYPES[t])
+            folder = cell + "/%s/*" % IMAGE_TYPES[t]
+            tifs = sorted([x for x in glob(folder)
                           if not x.endswith("Thumbs.db")])
+            logging.info("Found %g original files under %s" % (
+                         len(tifs), folder))
 
             with open(filepaths_file, 'a') as f:
                 for tif in tifs:
