0728e1e2859fcd659e8421374e3b96f8d0307cd9
Sebastien Besson <seb.besson@gmail.com>
2018-03-21 15:24:39 +0000

    Add logging

--- scripts/create_annotation.py
+++ scripts/create_annotation.py
@@ -1,26 +1,42 @@
 #!/usr/bin/env python
 # Generate annotation.csv from original assays file
 
+import logging
+import os
+import os.path
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas
 
 IMAGEFILEPATH = 'Comment [Image File Path]'
 CELL_PATTERN = '^mitotic_cell_atlas/(\w+)/.*$'
 TYPE_PATTERN = '^.*/(\w+)tif$'
+DEBUG = int(os.environ.get("DEBUG", logging.INFO))
+
+logging.basicConfig(level=DEBUG)
 
 # Read the assays file
-df = pandas.read_csv('experimentA/idr0041-assays.txt', sep='\t')
+scripts_dir = os.path.dirname(os.path.realpath(__file__))
+assays_file = os.path.join(
+    scripts_dir, "..", "experimentA", "idr0041-assays.txt")
+logging.info("Reading %s" % assays_file)
+df = pandas.read_csv(assays_file, sep='\t')
 
 # Generate the dataset and image name columns
+logging.debug("Generating dataset and image name columns")
 df['Dataset Name'] = df[IMAGEFILEPATH].str.extract(CELL_PATTERN) + \
     '_' + df[IMAGEFILEPATH].str.extract(TYPE_PATTERN)
 df['Image Name'] = df['Image File'].str[:-4]
 
 # Reorder columns to start with Dataset and Image names
+logging.debug("Generating dataset and image name columns")
 cols = df.columns.tolist()
 cols = cols[-1:] + cols[:-1]
 cols.insert(0, cols.pop(cols.index('Dataset Name')))
 df = df[cols]
 
 # Create annotation
-df.to_csv('../experimentA/idr0041-experimentA-annotation.csv', sep=',',
-             index=False)
+csv_file = os.path.join(
+    scripts_dir, "..", "experimentA", "idr0041-annotation.csv")
+logging.info("Generating %s" % csv_file)
+df.to_csv(csv_file, sep=',', index=False)
