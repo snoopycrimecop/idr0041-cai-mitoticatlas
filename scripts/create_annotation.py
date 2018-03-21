3ce3b00fd1b24ef7e9570129701feec09b9a8d27
Sebastien Besson <seb.besson@gmail.com>
2018-03-21 15:18:24 +0000

    Remove filtering

--- scripts/create_annotation.py
+++ scripts/create_annotation.py
@@ -9,19 +9,18 @@ TYPE_PATTERN = '^.*/(\w+)tif$'
 
 # Read the assays file
 df = pandas.read_csv('experimentA/idr0041-assays.txt', sep='\t')
-kif11 = df[df[IMAGEFILEPATH].str.contains('KIF11')]
 
 # Generate the dataset and image name columns
-kif11['Dataset Name'] = kif11[IMAGEFILEPATH].str.extract(CELL_PATTERN) + \
-    '_' + kif11[IMAGEFILEPATH].str.extract(TYPE_PATTERN)
-kif11['Image Name'] = kif11['Image File'].str[:-4]
+df['Dataset Name'] = df[IMAGEFILEPATH].str.extract(CELL_PATTERN) + \
+    '_' + df[IMAGEFILEPATH].str.extract(TYPE_PATTERN)
+df['Image Name'] = df['Image File'].str[:-4]
 
-# Reorder columns
-cols = kif11.columns.tolist()
+# Reorder columns to start with Dataset and Image names
+cols = df.columns.tolist()
 cols = cols[-1:] + cols[:-1]
 cols.insert(0, cols.pop(cols.index('Dataset Name')))
-kif11 = kif11[cols]
+df = df[cols]
 
 # Create annotation
-kif11.to_csv('../experimentA/idr0041-experimentA-annotation.csv', sep=',',
+df.to_csv('../experimentA/idr0041-experimentA-annotation.csv', sep=',',
              index=False)
