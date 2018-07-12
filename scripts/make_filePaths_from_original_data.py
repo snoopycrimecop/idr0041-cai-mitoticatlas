2ffa5c821d81f34f3e5abc666cab982bc1a3a8ed
Sebastien Besson <seb.besson@gmail.com>
2018-07-12 22:42:46 +0200

    Refactory file path generation script
    Separate filePaths discovery from filepath generation

--- scripts/make_filePaths_from_original_data.py
+++ scripts/make_filePaths_from_original_data.py
@@ -36,40 +36,36 @@ metadata_dir = os.path.dirname(os.path.realpath(__file__))
 filepaths_file = join(metadata_dir, "..", "experimentA",
                       "idr0041-experimentA-filePaths.tsv")
 
-# Delete tsv file to prevent appending
-if os.path.exists(filepaths_file):
-    logging.info("Deleting %s" % filepaths_file)
-    os.remove(filepaths_file)
-
 # List all assay folders found under base directory
-all_assays = [join(BASE_DIRECTORY, x) for x in os.listdir(BASE_DIRECTORY)]
-all_assays = sorted(filter(os.path.isdir, all_assays))
-logging.info("Found %g folders under %s" % (len(all_assays), BASE_DIRECTORY))
+original_folders = [join(BASE_DIRECTORY, x) for x in
+                    os.listdir(BASE_DIRECTORY)]
+original_folders = sorted(filter(os.path.isdir, original_folders))
+logging.info("Found %g folders under %s" % (len(original_folders),
+             BASE_DIRECTORY))
 
 # Loop over subset of assay folders delimited by START and STOP
-assays = all_assays[START - 1:STOP]
-nfiles = 0
-nfolders = 0
-logging.info("Generating %s for %g folders" % (filepaths_file, len(assays)))
-for assay in assays:
-    logging.debug("Finding cells under %s" % assay)
+files = []
+folders = original_folders[START - 1:STOP]
+logging.info("Generating %s for %g folders" % (filepaths_file, len(folders)))
+for folder in folders:
+    logging.debug("Finding cells under %s" % folder)
     # Retrieve individual cell per assays excluding calibration folders
-    cells = [x for x in glob(assay + "/*") if not x.endswith("Calibration")]
+    cells = [x for x in glob(folder + "/*") if not x.endswith("Calibration")]
     for cell in cells:
         for t in IMAGE_TYPES:
-            folder = cell + "/%s/*" % IMAGE_TYPES[t]
-            tifs = sorted([x for x in glob(folder)
+            subfolder = cell + "/%s/*" % IMAGE_TYPES[t]
+            tifs = sorted([x for x in glob(subfolder)
                           if not x.endswith("Thumbs.db")])
             logging.debug("Found %g original files under %s" % (
-                         len(tifs), folder))
-            nfolders = nfolders + 1
-            with open(filepaths_file, 'a') as f:
-                for tif in tifs:
-                    f.write("Dataset:name:%s\t%s\t%s\n" % (
-                            basename(assay) + "_%s" % t,
-                            tif,
-                            basename(cell) + basename(tif)[-10:-4]))
-                    nfiles = nfiles + 1
+                         len(tifs), subfolder))
 
-logging.info("Listed %g files to import, located in %g folders" %
-             (nfiles, nfolders))
+            assay = basename(folder) + "_%s" % t
+            for tif in tifs:
+                imagename = basename(cell) + basename(tif)[-10:-4]
+                files.append((assay, tif, imagename))
+
+logging.info("Listed %g files to import" % len(files))
+
+with open(filepaths_file, 'w') as f:
+    for i in files:
+        f.write("Dataset:name:%s\t%s\t%s\n" % (i[0], i[1], i[2]))
