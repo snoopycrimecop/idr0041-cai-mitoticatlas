347c1fcabe10c4ccbd86a38c46885a1d271f7769
Sebastien Besson <seb.besson@gmail.com>
2018-08-06 21:25:04 +0100

    add log in command

--- scripts/update_v101.sh
+++ scripts/update_v101.sh
@@ -7,6 +7,9 @@
 export omero=/opt/omero/server/OMERO.server/bin/omero
 projectId=404
 
+# Log in to local server
+$omero login demo@localhost
+
 # Delete existing map annotations
 $omero metadata populate --batch 100 --wait 2000 --context deletemap --cfg experimentA/idr0041-experimentA-bulkmap-config.yml Project:$projectId --report
 
